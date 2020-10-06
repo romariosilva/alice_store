567d6cee2a46e0e3ee6bc9ca8ef3d2630b780f2f
Romario Alysson <romarioalysson1@gmail.com>
2020-10-06 09:09:14 -0300

    building product_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:alice_store/models/product.dart';
 import 'package:alice_store/models/product_manager.dart';
 import 'package:alice_store/models/user_manager.dart';
 import 'package:alice_store/screens/base/base_screen.dart';
 import 'package:alice_store/screens/login/login_screen.dart';
+import 'package:alice_store/screens/product/product_screen.dart';
 import 'package:alice_store/screens/signup/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -50,6 +52,13 @@ class MyApp extends StatelessWidget {
             case '/signup':
               return MaterialPageRoute(
                 builder: (_) => SignUpScreen()
+              );
+              break;
+            case '/product':
+              return MaterialPageRoute(
+                builder: (_) => ProductScreen(
+                  settings.arguments as Product
+                )
               );
               break;
             case '/base':
--- lib/screens/products/components/product_list_tile.dart
+++ lib/screens/products/components/product_list_tile.dart
@@ -9,54 +9,59 @@ class ProductListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4)
-      ),
-      child: Container(
-        height: 100,
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Image.network(product.images.first),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    product.name,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4),
-                    child: Text(
-                      'A partir de',
-                      style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 12
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).pushNamed('/product', arguments: product);
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4)
+        ),
+        child: Container(
+          height: 100,
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Image.network(product.images.first),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600
                       ),
                     ),
-                  ),
-                  Text(
-                    'R\$ 19.99',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w800,
-                      color: Theme.of(context).primaryColor
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Text(
+                        'A partir de',
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 12
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    Text(
+                      'R\$ 19.99',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Theme.of(context).primaryColor
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
