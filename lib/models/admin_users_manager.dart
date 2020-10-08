f73da9dd57dee7c7d149c23e9404d3d99ff457e5
Romario Alysson <romarioalysson1@gmail.com>
2020-10-07 23:50:23 -0300

    Taking users firebase

--- lib/models/admin_users_manager.dart
+++ lib/models/admin_users_manager.dart
@@ -1,34 +1,45 @@
+import 'dart:async';
+
 import 'package:alice_store/models/user_data.dart';
 import 'package:alice_store/models/user_manager.dart';
-import 'package:faker/faker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class AdminUsersManager extends ChangeNotifier{
 
   List<UserData> users = [];
 
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  StreamSubscription _subscription;
+
   void updateUser(UserManager userManager){
+    _subscription?.cancel();
     if(userManager.adminEnabled){
       _listenToUsers();
+    } else {
+      users.clear();
+      notifyListeners();
     }
   }
 
+  //Buscando todos os usuários
   void _listenToUsers(){
-    const faker = Faker();
-
-    for(int i = 0; i < 1000; i++){
-      users.add(UserData(
-        name: faker.person.name(),
-        email: faker.internet.email()
-      ));
-    }
-
-    users.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
-    
-    notifyListeners();
+    _subscription = firestore.collection('users').snapshots().listen((snapshot){
+      users = snapshot.docs.map((e) => UserData.fromDocument(e)).toList();
+      users.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+      notifyListeners();
+    });
   }
 
   //Pegando o nome dos usuários
   List<String> get names => users.map((e) => e.name).toList();
+
+  @override
+  void dispose() {
+    // O '?' significa que se o _subscription for null então não chama o cancel
+    _subscription?.cancel();
+    super.dispose();
+  }
 
 }
--- lib/screens/admin_users/admin_users_screen.dart
+++ lib/screens/admin_users/admin_users_screen.dart
@@ -1,3 +1,4 @@
+import 'package:alice_store/common/custom_drawer/custom_drawer.dart';
 import 'package:alice_store/models/admin_users_manager.dart';
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ class AdminUsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
       appBar: AppBar(
         title: const Text('Usuários'),
         centerTitle: true,
