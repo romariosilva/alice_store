71ca14b16a45fedd90a7bd17ac87952de1aab208
Romario Alysson <romarioalysson1@gmail.com>
2020-10-16 14:31:09 -0300

    Showing Panel Status

--- lib/screens/admin_orders/admin_orders_screen.dart
+++ lib/screens/admin_orders/admin_orders_screen.dart
@@ -3,10 +3,15 @@ import 'package:alice_store/common/custom_icon_button.dart';
 import 'package:alice_store/common/empty_card.dart';
 import 'package:alice_store/common/order/order_tile.dart';
 import 'package:alice_store/models/admin_orders_manager.dart';
+import 'package:alice_store/models/order.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class AdminOrdersScreen extends StatelessWidget {
+
+  final PanelController panelController = PanelController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,52 +24,99 @@ class AdminOrdersScreen extends StatelessWidget {
         builder: (_, ordersManager, __){
           final filteredOrders = ordersManager.filteredOrders;
 
-          return Column(
-            children: <Widget>[
-              if(ordersManager.userFilter != null)
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          'Pedidos de ${ordersManager.userFilter.name}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w800,
-                            color: Colors.white,
+          return SlidingUpPanel(
+            controller: panelController,
+            body: Column(
+              children: <Widget>[
+                if(ordersManager.userFilter != null)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            'Pedidos de ${ordersManager.userFilter.name}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w800,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
+                        CustomIconButton(
+                          iconData: Icons.close,
+                          color: Colors.white,
+                          onTap: (){
+                            ordersManager.setUserFilter(null);
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                if(filteredOrders.isEmpty)
+                  const Expanded(
+                    child: EmptyCard(
+                      title: 'Nenhuma venda realizada!',
+                      iconData: Icons.border_clear,
+                    ),
+                  )
+                else
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: filteredOrders.length,
+                      itemBuilder: (_, index){
+                        return OrderTile(
+                          filteredOrders[index],
+                          showControls: true,
+                        );
+                      }
+                    ),
+                  )
+              ],
+            ),
+            minHeight: 40,
+            maxHeight: 240,
+            panel: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    if(panelController.isPanelClosed){
+                      panelController.open();
+                    } else {
+                      panelController.close();
+                    }
+                  },
+                  child: Container(
+                    height: 40,
+                    color: Colors.white,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Filtros',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
                       ),
-                      CustomIconButton(
-                        iconData: Icons.close,
-                        color: Colors.white,
-                        onTap: (){
-                          ordersManager.setUserFilter(null);
-                        },
-                      )
-                    ],
+                    ),
                   ),
                 ),
-              if(filteredOrders.isEmpty)
-                const Expanded(
-                  child: EmptyCard(
-                    title: 'Nenhuma venda realizada!',
-                    iconData: Icons.border_clear,
-                  ),
-                )
-              else
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: filteredOrders.length,
-                    itemBuilder: (_, index){
-                      return OrderTile(
-                        filteredOrders[index],
-                        showControls: true,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: Status.values.map((s){
+                      return CheckboxListTile(
+                        title: Text(Order.getStatusText(s)),
+                        dense: true,
+                        value: true, 
+                        onChanged: (v){
+
+                        }
                       );
-                    }
+                    }).toList(),
                   ),
                 )
-            ],
+              ],
+            ),
           );
         },
       ),
