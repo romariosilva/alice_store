e5d1a02586beb6b39f5e3efbaf27fab93e17afb7
Romario Alysson <romarioalysson1@gmail.com>
2020-10-17 10:20:41 -0300

    Creating object Store

--- lib/models/stores_manager.dart
+++ lib/models/stores_manager.dart
@@ -1,18 +1,21 @@
+import 'package:alice_store/models/store.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class StoresManager extends ChangeNotifier {
 
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  List<Store> stores = [];
+
   StoresManager(){
     _loadStoreList();
   }
 
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
   Future<void> _loadStoreList() async {
-    final snapshot = await firestore.collection('stores').get();
+    final snapshot = await firestore.collection('store').get();
 
-    print(snapshot.docs.first.data);
+    stores = snapshot.docs.map((e) => Store.fromDocument(e)).toList();
 
     notifyListeners();
   }
